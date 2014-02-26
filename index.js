1538f7b492a02b656765d4b2a8dff1d1a6bd3c51
John Syrinek <john.syrinek@gmail.com>
2014-02-26 16:29:19 -0600

    Adds a fix for excessive caching in IE 11 (adds additional Cache-Control headers)

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-module.exports = function(request) {
-	request.headers = {
-		'X-Requested-With': 'XMLHttpRequest',
-		'Cache-Control': 'no-cache'
-	};
+module.exports = function (request) {
+	request.set('X-Requested-With', 'XMLHttpRequest');
+	request.set('Cache-Control', 'no-cache,no-store,must-revalidate,max-age=-1');
 	return request;
 }
+module.exports.with_query_strings = function(request) {
+	request._query = [Date.now().toString()]
+	return module.exports(request);
+};
